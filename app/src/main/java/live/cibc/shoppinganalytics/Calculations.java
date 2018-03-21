36f82fff5ae360c8417a29434943568c9a30bb7d
Leslie Xin <leslie.xin@cibc.live>
2018-03-21 15:12:00 -0400

    removed useless folder

--- app/src/main/java/live/cibc/shoppinganalytics/Calculations.java
+++ app/src/main/java/live/cibc/shoppinganalytics/Calculations.java
@@ -13,18 +13,18 @@ public class Calculations {
 
 
     public static void getProperties(){
-        try {
-
-            
-            Properties prop = new Properties();
-            prop.load(App.class.getClassLoader().getResourceAsStream("config.properties"));
-
-            System.out.println(prop.getProperty("database"));
-            System.out.println(prop.getProperty("dbuser"));
-            System.out.println(prop.getProperty("dbpassword"));
-
-        } catch(IOException ex) {
-                ex.printStackTrace();
-        }
+//        try {
+//
+//
+//            Properties prop = new Properties();
+//            prop.load(App.class.getClassLoader().getResourceAsStream("config.properties"));
+//
+//            System.out.println(prop.getProperty("database"));
+//            System.out.println(prop.getProperty("dbuser"));
+//            System.out.println(prop.getProperty("dbpassword"));
+//
+//        } catch(IOException ex) {
+//                ex.printStackTrace();
+//        }
     }
 }
