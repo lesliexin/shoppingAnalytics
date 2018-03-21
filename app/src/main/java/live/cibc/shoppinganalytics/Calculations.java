e1e78ba4a68ac1207d6e0a8407f7f841aedce6e1
Leslie Xin <leslie.xin@cibc.live>
2018-03-21 15:10:15 -0400

    Added properties files and created new classes

---
+++ app/src/main/java/live/cibc/shoppinganalytics/Calculations.java
@@ -0,0 +1,30 @@
+package live.cibc.shoppinganalytics;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by lesliexin on 2018-03-21.
+ */
+
+public class Calculations {
+
+
+
+
+    public static void getProperties(){
+        try {
+
+            
+            Properties prop = new Properties();
+            prop.load(App.class.getClassLoader().getResourceAsStream("config.properties"));
+
+            System.out.println(prop.getProperty("database"));
+            System.out.println(prop.getProperty("dbuser"));
+            System.out.println(prop.getProperty("dbpassword"));
+
+        } catch(IOException ex) {
+                ex.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/live/cibc/shoppinganalytics/Retailer.java
@@ -0,0 +1,8 @@
+package live.cibc.shoppinganalytics;
+
+/**
+ * Created by lesliexin on 2018-03-21.
+ */
+
+public class Retailer {
+}
